c6ccfcde69e3e18a03b0fadda9eb65d73a9629a5
Walid Saifi <saifiwalid21@gmail.com>
2024-06-07 17:38:12 +0100

    Update userController.php

--- projet/app/Http/Controllers/API/userController.php
+++ projet/app/Http/Controllers/API/userController.php
@@ -69,7 +69,7 @@ class userController extends Controller
     {
         $user = User::findOrFail($id);
         return response()->json($user, 200);
-        
+
     }
     public function indexToutUser()
     {
