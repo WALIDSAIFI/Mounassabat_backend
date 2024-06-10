668738de7e0cbd3e3f837f1e661ee716268a1f39
Walid Saifi <saifiwalid21@gmail.com>
2024-06-10 15:41:57 +0100

    Update api.php

--- projet/routes/api.php
+++ projet/routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
@@ -26,13 +27,17 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
 
 });
-Route::get('/user/{id}', [UserController::class, 'indexUser']);
-Route::get('/user', [UserController::class, 'indexToutUser']);
-Route::post('/usersBLOK/{id}', [AdminController::class, 'blockUser']);
-Route::get('/nombre/Sixjour',[AdminController::class, 'userCreationsLastSixDays']);
-Route::get('/nombreBloker',[AdminController::class, 'countBlockedAndUnblockedUsers']);
-Route::get('/unbolkerUser/{id}',[AdminController::class, 'unblockUser']);
-Route::get('/nombreutilisateurBloker',[AdminController::class, 'nombreBolker']);
-Route::get('/nombreutilisateurNomBloker',[AdminController::class, 'nombreNomBloker']);
-Route::get('/userBloker',[AdminController::class, 'utilisateurBloker']);
-Route::get('/userNomBloker',[AdminController::class,'utilisateurNomBloker']);
+
+
+Route::middleware(['admin'])->group(function (){
+    Route::get('/user/{id}', [UserController::class, 'indexUser']);
+    Route::get('/user', [UserController::class, 'indexToutUser']);
+    Route::post('/usersBLOK/{id}', [AdminController::class, 'blockUser']);
+    Route::get('/nombre/Sixjour',[AdminController::class, 'userCreationsLastSixDays']);
+    Route::get('/nombreBloker',[AdminController::class, 'countBlockedAndUnblockedUsers']);
+    Route::get('/unbolkerUser/{id}',[AdminController::class, 'unblockUser']);
+    Route::get('/nombreutilisateurBloker',[AdminController::class, 'nombreBolker']);
+    Route::get('/nombreutilisateurNomBloker',[AdminController::class, 'nombreNomBloker']);
+    Route::get('/userBloker',[AdminController::class, 'utilisateurBloker']);
+    Route::get('/userNomBloker',[AdminController::class,'utilisateurNomBloker']);
+});
